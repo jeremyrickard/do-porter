123ee56ef4c2f7dc466f3e614b508daef694bfe1
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-09-05 12:33:19 -0600

    fix typo

--- waitForIP.sh
+++ waitForIP.sh
@@ -4,7 +4,7 @@ echo "Checking IP for $SERVICE_NAME"
 EXTERNAL_IP=''
 while [ -z $EXTERNAL_IP ]; do
   echo "Waiting for end point..."
-  EXTERNAL_IP=$(kubectl get svc $SERVICE_NAME --namesapce $NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  EXTERNAL_IP=$(kubectl get svc $SERVICE_NAME --namespace $NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   echo $EXTERNAL_IP
   sleep 10
 done
